272d92766ba89afa3c59da0323a24e18ba90a465
宗汰朗 光山 <oibc36179@ojs.ac.jp>
2025-04-23 10:08:08 +0900

    インチからメートルに変換

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    static class IntchConverter {
+
+        const double ratio = 0.0254;
+
+        //メートルからフィートを求める
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+        //フィートからメートルを求める
+        public static double ToMeter(double feet) {
+            return feet * ratio;
+        }
+    }
+}
