fb75cd38776c5617e7ee80a43450859f35f673bf
宗汰朗 光山 <oibc36179@ojs.ac.jp>
2025-05-09 14:14:55 +0900

    売上しゅうけいプログラム完成

---
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    //売上集計クラス
+    public class SalesCounter {
+        private readonly List<Sale> _sales;
+
+        //コンストラクタ
+        public SalesCounter(List<Sale> sales) {
+            _sales = sales;
+        }
+
+        //店舗別売上を求める
+        public Dictionary<string, int> GetPerStoreSales() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Sale sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ProductCategory] += sale.Amount;
+                else
+                    dict[sale.ProductCategory] = sale.Amount;
+
+            }
+            return dict;
+        }
+    }
+}
