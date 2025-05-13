f8d51e6e4b76c5a8f3d362d28d7bce431147b0f5
宗汰朗 光山 <oibc36179@ojs.ac.jp>
2025-05-13 11:26:50 +0900

    ２－１～2-4

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -2,8 +2,8 @@
     internal class Program {
         static void Main(string[] args) {
             var sales = new SalesCounter(@"data\sales.csv");
-            IDictionary<string, int> amountsPerStore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string, int> obj in amountsPerStore) {
+            var amountsPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key}{obj.Value}");
             }
         }
---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01{
+    //2.1.1
+    public class Song {
+        public string Title { get; private set; }= string.Empty;
+        public string ArtistName { get; private set; } = string.Empty;
+        public int Length { get; private set; } 
+
+
+        //2.1.2
+        //コンストラクタ
+        public Song(string title, string artistName, int length) {
+            this.Title = title;
+            this.ArtistName = artistName;
+            this.Length = length;
+
+
+            
+
+
+
+
+        }
+    }
+
+
+}
