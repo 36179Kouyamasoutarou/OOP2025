804a01a77f8acc6b03b7421991957b9c15b5a098
宗汰朗 光山 <oibc36179@ojs.ac.jp>
2025-06-10 11:25:11 +0900

    ５－２－４完成

--- Chapter05/演習問題/Exercise02/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Exercise02/Program.cs
@@ -1,30 +1,5 @@
 ﻿using Exercise01;
 
-namespace Exercise02 {
-    internal class Program {
-        static void Main(string[] args) {
-            // 5.2.1
-            var ymCollection = new YearMonth[] {
-                new YearMonth(1980, 1),
-                new YearMonth(1990, 4),
-                new YearMonth(2000, 7),
-                new YearMonth(2010, 9),
-                new YearMonth(2024, 12),
-            };
-
-            Console.WriteLine("5.2.2");
-            Exercise2(ymCollection);
-
-            Console.WriteLine("5.2.4");
-            Exercise4(ymCollection);
-
-
-            Console.WriteLine("5.2.5");
-            Exercise5(ymCollection);
-        }
-        //5.2.2
-        private static void Exercise2(YearMonth[] ymCollection) {
-            using Exercise01;
 
 namespace Exercise02 {
         internal class Program {
@@ -80,12 +55,8 @@ namespace Exercise02 {
             //5.2.5
             private static void Exercise5(YearMonth[] ymCollection) {
                 var newYmCollection = new YearMonth[ymCollection.Length];
-                for (int i = 0; i < ymCollection.Length; i++) {
-                    var ym = ymCollection[i];
-                    newYmCollection[i] = new YearMonth(ym.Year + 1, ym.Month);
-                }
-                foreach (var ym in newYmCollection) {
-                    Console.WriteLine(ym);  
+
+                
                 }
             }
         }
