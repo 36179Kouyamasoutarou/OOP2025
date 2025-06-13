e3ad72130d18a247909d5d595e81e8ba4af54b70
宗汰朗 光山 <oibc36179@ojs.ac.jp>
2025-06-13 13:44:02 +0900

    Exercise01完成

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Exercise01 {
+﻿namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
             var text = "Cozy lummox gives smart squid who asks for job pen";
@@ -7,36 +6,43 @@ namespace Exercise01 {
             Exercise1(text);
             Console.WriteLine();
 
+
             Exercise2(text);
 
         }
 
         private static void Exercise1(string text) {
-            var dict = new Dictionary<Char, int>();
-
+            var dict = new Dictionary<char, int>();
             foreach (var c in text.ToUpper()) {
-                // 'A' … 0x41
-                //  :       :
-                // 'Z' … 0x5a
-
-
-
-                //④アルファベットならディクショナリに登録
-                //  登録済み：valueをインクリメント
-                //  未登録：valueに1を設定
-
-
+                if ('A' <= c && c <= 'Z') {
+                    if (dict.ContainsKey(c)) {
+                        dict[c]++;
+                    } else {
+                        dict[c] = 1;
+                    }
+                }
             }
-
-
-
-
-            //⑥すべての文字が読み終わったら、アルファベット順に並び替えて出力
-
+            foreach (var item in dict.OrderBy(c => c.Key)) {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
         }
 
         private static void Exercise2(string text) {
-            
+            var dict = new SortedDictionary<char, int>();
+
+            foreach (var c in text.ToUpper()) {
+                if ('A' <= c && c <= 'Z') {
+                    if (dict.ContainsKey(c)) {
+                        dict[c]++;
+                    } else {
+                        dict[c] = 1; //未登録：valueに１を設定
+                    }
+                }
+            }
+            //⑥すべての文字が読み終わったら出力
+            foreach (var item in dict) {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
         }
     }
 }
