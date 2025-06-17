ab0eb76d3c471e3b8c725a8666b6686d4ddc88be
宗汰朗 光山 <oibc36179@ojs.ac.jp>
2025-06-17 11:18:24 +0900

    設問1_ Student クラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        //学生の名前
+        public string Name { get; set; }= string.Empty;
+        //科目名
+        public string Subject { get; set; } = string.Empty;
+        //引数
+        public int Score { get; set; }
+
+
+    }
+}
