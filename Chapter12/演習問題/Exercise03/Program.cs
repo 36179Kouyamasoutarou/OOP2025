f3dac5a22ea7ef298881e77fb3b4e61fdd460c93
宗汰朗 光山 <oibc36179@ojs.ac.jp>
2025-07-15 15:57:59 +0900

    Exercise03　完成

--- Chapter12/演習問題/Exercise03/Program.cs
+++ Chapter12/演習問題/Exercise03/Program.cs
@@ -1,16 +1,39 @@
-﻿namespace Exercise03 {
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
             var employees = Deserialize("employees.json");
             ToXmlFile(employees);
         }
 
-        static Employee[] Deserialize(string filePath) { 
+        static Employee[] Deserialize(string filePath) {
+            var opitions = new JsonSerializerOptions {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                WriteIndented = true
+
+            };
+            var text = File.ReadAllText(filePath);
+            var employees = JsonSerializer.Deserialize<Employee[]>(text, opitions);
+            return employees ?? [];
           
             
         }
 
-        static void ToXmlFile(Employee[] employees) { 
+        static void ToXmlFile(Employee[] employees) {
+            using (var writer = XmlWriter.Create("employees.xml")) {
+
+                XmlRootAttribute xRoot = new XmlRootAttribute {
+                    ElementName = "Employees"
+                
+            };
+            var serializer = new XmlSerializer(employees.GetType(),xRoot);
+            serializer.Serialize(writer, employees);
 
 
         }
