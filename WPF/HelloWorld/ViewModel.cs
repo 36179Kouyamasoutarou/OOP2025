c43a1d738a9e6656424b0abda7d912a4815941f4
宗汰朗 光山 <oibc36179@ojs.ac.jp>
2025-10-02 14:30:14 +0900

    InotifyPropertyChangedの場合

---
+++ WPF/HelloWorld/ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel() {
+            ChangeMessageCommand = new DelegateCommand(
+                () => GreetingMessage = "Paipai-World");
+
+
+        }
+        private string _greetingMessage = "HelloMessage";
+        public string GreetingMessage {
+            get=>_greetingMessage;
+            set {
+                if (_greetingMessage != value) {
+                    _greetingMessage = value;
+                    PropertyChanged?.Invoke(
+                        this, new PropertyChangedEventArgs(nameof(GreetingMessage)));
+                }
+                }
+            }
+
+        public DelegateCommand ChangeMessageCommand { get; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
