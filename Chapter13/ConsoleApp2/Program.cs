86119113a448888721da2edd1570db0cb78eff17
宗汰朗 光山 <oibc36179@ojs.ac.jp>
2025-10-29 10:08:38 +0900

    1-3

---
+++ Chapter13/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Section01{
+    internal class Program {
+        static void Main(string[] args) {
+            var price = Library.Books
+                .Where(b => b.CategoryId == 1)
+                .Max(b => b.Price);
+            Console.WriteLine(price);
+
+            Console.WriteLine();
+
+            var book = Library.Books
+                .Where(b => b.)
+        }
+    }
+}
---
+++ Chapter13/Exercise01/Program.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+            var book = Library.Books
+                    .MaxBy(b => b.Price);
+            Console.WriteLine(book);
+        }
+
+        private static void Exercise1_3() {
+            var results = Library.Books
+                .GroupBy(b => b.PublishedYear)
+                .OrderBy(b => b.Key)
+                .Select(b => new {
+                    PublishedYear = b.Key,
+                    Count = b.Count(),
+                });
+        }
+
+        private static void Exercise1_4() {
+            
+        }
+
+        private static void Exercise1_5() {
+            
+        }
+
+        private static void Exercise1_6() {
+            
+        }
+
+        private static void Exercise1_7() {
+        
+        }
+
+        private static void Exercise1_8() {
+            
+        }
+    }
+}
